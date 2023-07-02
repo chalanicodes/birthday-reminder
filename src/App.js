3dffada144f24b57f452dcbe0b3d6a5ca072f5c8
Chalani Dissanayaka <chalani@192-168-1-3.tpgi.com.au>
2023-07-02 23:35:15 +1000

    updated App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [people, setPeople] = useState(data);
   return <main>
     <section className='container'>
-      <h3>{people.length} birthdays today</h3>
+      <h3>We have {people.length} birthdays today</h3>
       <List people={people} />
       <button onClick={() => setPeople([])}>
         clear all </button>
